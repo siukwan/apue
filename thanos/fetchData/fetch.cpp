1e69a127b53dce5513254a072897d76d328b5f72
siukwanCentOS <pypy39353@qq.com>
2016-03-05 22:55:20 +0800

    成功抓取数据 g++ $(curl-config --libs) fetch.cpp

--- thanos/fetchData/fetch.cpp
+++ thanos/fetchData/fetch.cpp
@@ -1,7 +1,9 @@
+// g++ $(curl-config --libs) fetch.cpp 
+
 #include <stdio.h>  
 #include <stdlib.h>  
 #include <string.h>  
-#include <curl/curl.h> 
+#include <curl/curl.h>  //centos yum install libcurl-devel 
 #include <iostream>
 using namespace std;
 size_t write_data(void *buffer,size_t size,size_t nmemb,void *userp);
@@ -20,11 +22,11 @@ int main(int argc, char *argv[])
 	}
 
 	curl = curl_easy_init();
-	curl_easy_setopt(curl,CURLOPT_URL,"https://www.baidu.com");
+	curl_easy_setopt(curl,CURLOPT_URL,"http://hq.sinajs.cn/?list=hf_XAG,hf_XAU");
 	curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,write_data);
 	curl_easy_setopt(curl,CURLOPT_WRITEDATA,fptr);
 
-	res = curl_easy_preform(curl);
+	res = curl_easy_perform(curl);
 	cout<<res<<endl;
 	curl_easy_cleanup(curl);
 	return 0;
@@ -34,4 +36,5 @@ size_t write_data(void *buffer,size_t size, size_t nmemb, void *userp)
 {
 	FILE *fptr = (FILE*) userp;
 	fwrite(buffer,size,nmemb,fptr);
+	printf("%s\n",(char*)buffer);
 }
