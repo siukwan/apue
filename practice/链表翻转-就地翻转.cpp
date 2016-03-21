89a27346b95c605e47a87fce56d7d3298a25ed71
siukwan <siukwan@users.noreply.github.com>
2016-03-21 15:58:28 +0800

    链表翻转-就地翻转.cpp

---
+++ practice/链表翻转-就地翻转.cpp
@@ -0,0 +1,28 @@
+﻿/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		//就地反转
+		if (head == NULL || head->next == NULL) return head;
+
+		ListNode* pre = NULL;
+		ListNode* next = NULL;
+
+		while (head)
+		{
+			next = head->next;
+			head->next = pre;
+			pre = head;
+			head = next;
+		}
+
+		return pre;
+	}
+};
