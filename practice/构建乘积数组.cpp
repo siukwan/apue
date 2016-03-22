d0f5fa412190a1cc8e044b6c3cb104de02b6704e
siukwan <siukwan@users.noreply.github.com>
2016-03-22 21:08:41 +0800

    构建乘积数组

---
+++ practice/构建乘积数组.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+	vector<int> multiply(const vector<int>& A) {
+		vector<int> B(A.size(), 1);
+		int tmp = 1;
+		for (int i = 1; i<A.size(); ++i)
+		{
+			tmp *= A[i - 1];
+			B[i] *= tmp;
+		}
+
+		tmp = 1;
+		for (int i = A.size() - 2; i >= 0; --i)
+		{
+			tmp *= A[i + 1];
+			B[i] *= tmp;
+		}
+		return B;
+	}
+};
