ab0115557d23002dbe590449970708141cc310c3
siukwan <siukwan@users.noreply.github.com>
2016-03-26 22:06:04 +0800

    奇偶位交换

---
+++ practice/奇偶位交换.cpp
@@ -0,0 +1,18 @@
+﻿/*
+题目描述
+
+请编写程序交换一个数的二进制的奇数位和偶数位。（使用越少的指令越好）
+给定一个int x，请返回交换后的数int。
+测试样例：
+10
+返回：5
+
+*/
+
+class Exchange {
+public:
+	int exchangeOddEven(int x) {
+		// write code here
+		return (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+	}
+};
